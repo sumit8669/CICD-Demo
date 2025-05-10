612aac78019e1def8b7614a13a165b7703117cf8
“sumit8669” <sumit2628r@gmail.com>
2025-05-10 07:19:49 +0530

    code commit

---
+++ src/main/java/com/sumit/cicd_demo/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.sumit.cicd_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/home")
+    public String homePage(){
+        return "WElCOME TO HOME PAGE";
+    }
+}
---
+++ src/test/java/com/sumit/cicd_demo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.cicd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
